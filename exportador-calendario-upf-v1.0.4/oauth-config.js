cd1824a13bdca6046ba26304c690b57d03112dd7
arturguasch <arturguasch@gmail.com>
2026-09-03 00:27:05 +0200

    1.0.4

---
+++ exportador-calendario-upf-v1.0.4/oauth-config.js
@@ -0,0 +1,5 @@
+// Credencial OAuth tipo "Aplicacion web" (NO la de Extension de Chrome).
+// Tras crearla en Google Cloud, pega aqui su Client ID.
+window.UPF_OAUTH_CONFIG = {
+  webClientId: "771957706968-d60p0k5afl5l4kj7f6gm0al4buu0qu0s.apps.googleusercontent.com",
+};
